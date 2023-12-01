868f79f8885b069a48eaf13b73dd89f451d3d5e2
Tamás Kis <t.kis502@gmail.com>
2023-12-01 08:34:59 +0100

    replace the template literal with the function templateCard

--- js/app.js
+++ js/app.js
@@ -2,20 +2,29 @@ import days from "../data/data.js";
 
 const $container = document.querySelector(".js-container");
 
+function templateCard(day) {  // TODO
+    const card = document.createElement("div");
+    const front = document.createElement("div");
+    const back = document.createElement("div");
+
+    card.classList = "card";
+    front.classList = "card_content card_front";
+    back.classList = "card_content card_back";
+
+    front.innerHTML = `<h2>December ${day}.</h2>`
+    back.innerHTML = `<p>Már csak ${24 - day} nap van Karácsonyig</p>`
+
+    card.appendChild(front);
+    card.appendChild(back);
+    return card;
+}
+
 const render = () => {
     let html = "";
     for (let i = 1; i <= 24; i++) {
-        const template = `
-        <div class="card">
-            <div class="card_content card_front">December ${i}.</div>
-            <div class="card_content card_back">
-            Már csak ${24 - i} nap van Karácsonyig
-            </div>
-        </div>
-        `;
-        html += template;
+        const newCard = templateCard(i);
+        $container.appendChild(newCard);
     }
-    $container.innerHTML = html;
 };
 
 render();
