51d7ed4fa93f73401cd83189173ea691c359dc76
Tamás Kis <t.kis502@gmail.com>
2023-12-01 08:16:44 +0100

    replace section.innerHtml with a js code

--- js/app.js
+++ js/app.js
@@ -1,1 +1,21 @@
 import days from "../data/data.js";
+
+const $container = document.querySelector(".js-container");
+
+const render = () => {
+    let html = "";
+    for (let i = 1; i <= 24; i++) {
+        const template = `
+        <div class="card">
+            <div class="card_content card_front">December ${i}.</div>
+            <div class="card_content card_back">
+            Már csak ${24 - i} nap van Karácsonyig
+            </div>
+        </div>
+        `;
+        html += template;
+    }
+    $container.innerHTML = html;
+};
+
+render();
