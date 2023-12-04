ace71f87c3fb01d59981dde366e2142a8d94a23e
Tamás Kis <t.kis502@gmail.com>
2023-12-04 10:57:19 +0100

    use localStorage, add a button to close the windows,  use Date for early opening attempts

--- js/app.js
+++ js/app.js
@@ -1,6 +1,26 @@
 import data from "../data/data.js"; //TODO
 
 const $container = document.querySelector(".js-container");
+const $btn = document.querySelector(".js_btn");
+const date = new Date();
+const today = date.getDate(); //TODO
+
+let days;
+
+function saveToStorage() {
+    localStorage.setItem("data", JSON.stringify(days));
+}
+
+function initialData() {
+    const storageData = localStorage.getItem("data");
+    const localData = JSON.parse(storageData);
+
+    if (localData) {
+        days = localData;
+    } else {
+        days = data;
+    }
+}
 
 function templateCard(day) {  // TODO
     const card = document.createElement("div");
@@ -8,8 +28,14 @@ function templateCard(day) {  // TODO
     const back = document.createElement("div");
 
     card.classList = "card";
-    front.classList = "card_content card_front";
-    back.classList = "card_content card_back is-flipped";
+
+    if (day.isFlipped) {
+        front.classList = "card_content card_front is-flipped";
+        back.classList = "card_content card_back";
+    } else {
+        front.classList = "card_content card_front";
+        back.classList = "card_content card_back is-flipped";
+    }
 
     front.innerHTML = `<h2>December ${day.day}.</h2>`;
 
@@ -18,7 +44,7 @@ function templateCard(day) {  // TODO
     if (day.day === 24) {
         message = "Ma van szenteste";
     } else {
-        message = `Már csak ${24 - day.day} nap van Karácsonyig`;
+        message = `Már csak ${24 - day.day + 1} nap van Karácsonyig`;
     }
 
     back.innerHTML = `
@@ -40,19 +66,41 @@ function templateCard(day) {  // TODO
     card.appendChild(back);
 
     card.addEventListener("click", () => {
-        front.classList.toggle("is-flipped");
-        back.classList.toggle("is-flipped");
+
+        if (day.day <= today) {
+            front.classList.add("is-flipped");
+            back.classList.remove("is-flipped");
+
+            days[day.day - 1].isFlipped = true;
+
+            saveToStorage();
+        } else {
+            alert("Hooo-hooo-hooo ennek a dátumnak még nincs itt az ideje!")
+        }
     });
 
     return card;
 }
 
 const render = () => {
+    $container.innerHTML = "";  // TODO
+
     let html = "";
-    for (let day of data) {
+    for (let day of days) {
         const newCard = templateCard(day);
         $container.appendChild(newCard);
     }
 };
 
+$btn.addEventListener("click", () => {
+    for (let day of days) {
+        day.isFlipped = false;
+    }
+
+    localStorage.clear();
+
+    render();
+})
+
+initialData();
 render();
